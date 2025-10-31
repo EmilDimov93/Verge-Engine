4add5632a7eb7043741214073483746af3ad2720
EmilDimov93 <emildimovjr@gmail.com>
2025-10-31 16:17:44 +0200

    Fix FPS Manager bug

--- src/FPSManager.cpp
+++ src/FPSManager.cpp
@@ -1,19 +1,21 @@
 #include "FPSManager.h"
+#include <thread>
 
 void FPSManager::CorrectFrameTime()
 {
     while (true)
     {
-        std::chrono::duration<double> elapsed = std::chrono::high_resolution_clock::now() - timeAtStartOfFrame;
+        std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - timeAtStartOfFrame;
         if (elapsed.count() >= targetFrameTime)
         {
             break;
         }
+        std::this_thread::sleep_for(std::chrono::microseconds(50));
     }
 
-    currentFPS = static_cast<int>(1 / (std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - timeAtStartOfFrame).count()));
+    currentFPS = static_cast<int>(1 / (std::chrono::duration<double>(std::chrono::steady_clock::now() - timeAtStartOfFrame).count()));
 
-    timeAtStartOfFrame = std::chrono::high_resolution_clock::now();
+    timeAtStartOfFrame = std::chrono::steady_clock::now();
 }
 
 void FPSManager::SetTargetFPS(int targetFPS)

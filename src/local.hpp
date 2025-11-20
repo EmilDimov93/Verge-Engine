0754dbff65f435c8872eaebefa7f1c7b7e600fd8
EmilDimov93 <emildimovjr@gmail.com>
2025-11-20 10:52:40 +0200

    Add pragma once to local.hpp

--- src/local.hpp
+++ src/local.hpp
@@ -1,4 +1,6 @@
 // Copyright 2025 Emil Dimov
 // Licensed under the Apache License, Version 2.0
 
+#pragma once
+
 #define DEVELOPER_MODE false
--- src/version.hpp
+++ src/version.hpp
@@ -3,4 +3,4 @@
 
 #pragma once
 
-#define VERGE_ENGINE_VERSION 62
+#define VERGE_ENGINE_VERSION 63

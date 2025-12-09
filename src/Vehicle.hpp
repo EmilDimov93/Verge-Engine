c00aaed9d828967016a24b6f599651229b1a2d1e
EmilDimov93 <emildimovjr@gmail.com>
2025-12-09 18:34:29 +0200

    Add Vehicle.hpp file

---
+++ src/Vehicle.hpp
@@ -0,0 +1,16 @@
+// Copyright 2025 Emil Dimov
+// Licensed under the Apache License, Version 2.0
+
+#pragma once
+
+#include "Mesh.hpp"
+
+class Vehicle
+{
+public:
+    Vehicle(Mesh &&bodyMesh, Mesh &&tireMesh) : body(std::move(bodyMesh)), tire(std::move(tireMesh)){}
+
+private:
+    Mesh body;
+    Mesh tire;
+};
--- src/version.hpp
+++ src/version.hpp
@@ -3,4 +3,4 @@
 
 #pragma once
 
-#define VERGE_ENGINE_VERSION 87
+#define VERGE_ENGINE_VERSION 88

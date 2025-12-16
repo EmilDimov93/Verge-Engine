525409d08f1f5cec893e4943667b03c59781428b
EmilDimov93 <emildimovjr@gmail.com>
2025-12-16 19:30:29 +0200

    Add Camera.cpp and Camera.hpp; Add Camera class

---
+++ src/Camera.hpp
@@ -0,0 +1,12 @@
+// Copyright 2025 Emil Dimov
+// Licensed under the Apache License, Version 2.0
+
+#include "definitions.hpp"
+
+class Camera
+{
+public:
+private:
+    Position3 position;
+    Rotation3 rotation;
+};
--- src/version.hpp
+++ src/version.hpp
@@ -3,4 +3,4 @@
 
 #pragma once
 
-#define VERGE_ENGINE_VERSION 111
+#define VERGE_ENGINE_VERSION 112

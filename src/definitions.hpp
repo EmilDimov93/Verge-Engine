91d8d92e924484aec2be66c89a63745226a809a7
EmilDimov93 <emildimovjr@gmail.com>
2025-12-24 15:24:28 +0200

    Add glm overloads for custom Size, Position and Rotation types

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -16,30 +16,40 @@ struct Size2 {
     uint32_t w, h;
 
     constexpr Size2(uint32_t width = 0, uint32_t height = 0) : w(width), h(height) {}
+
+    operator glm::vec2() const { return {static_cast<float>(w), static_cast<float>(h)}; }
 };
 
 struct Size3 {
     uint32_t w, h, d;
 
     constexpr Size3(uint32_t width = 0, uint32_t height = 0, uint32_t depth = 0) : w(width), h(height), d(depth) {}
+
+    operator glm::vec3() const { return {static_cast<float>(w), static_cast<float>(h), static_cast<float>(d)}; }
 };
 
 struct Position2 {
     double x, y;
 
     constexpr Position2(double x_val = 0.0, double y_val = 0.0) : x(x_val), y(y_val) {}
+
+    explicit operator glm::dvec2() const { return {x, y}; }
 };
 
 struct Position3 {
     double x, y, z;
 
     constexpr Position3(double x_val = 0.0, double y_val = 0.0, double z_val = 0.0) : x(x_val), y(y_val), z(z_val) {}
+
+    explicit operator glm::dvec3() const { return {x, y, z}; }
 };
 
 struct Rotation3 {
     double pitch, yaw, roll;
 
     constexpr Rotation3(double pitch_val = 0.0, double yaw_val = 0.0, double roll_val = 0.0) : pitch(pitch_val), yaw(yaw_val), roll(roll_val) {}
+
+    explicit operator glm::dvec3() const { return {pitch, yaw, roll}; }
 };
 
 struct Vertex {
